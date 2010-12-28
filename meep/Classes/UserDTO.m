dca82a70cf64a6824cfe60802200eb0260990381
Alex Jarvis <alex@panaxiom.co.uk>
2010-12-28 16:13:56 +0000

    Added HUD with activity indicator for when the user presses the Register button.

---
+++ meep/Classes/UserDTO.m
@@ -0,0 +1,38 @@
+//
+//  UserDTO.m
+//  meep
+//
+//  Created by Alex Jarvis on 27/12/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserDTO.h"
+
+
+@implementation UserDTO
+
+@synthesize email;
+@synthesize password;
+@synthesize firstName;
+@synthesize lastName;
+@synthesize userName;
+@synthesize mobileNumber;
+
+- (void) dealloc {
+	[email release];
+	[password release];
+	[firstName release];
+	[lastName release];
+	[userName release];
+	[mobileNumber release];
+	[super dealloc];
+}
+
+
+-(NSString *)paramString {
+	
+	return [NSString stringWithFormat:@"user.email=%@&user.password=%@&user.firstName=%@&user.lastName=%@&user.serviceName=%@&user.telephone=%@", email, password, firstName, lastName, userName, mobileNumber];
+	
+}
+
+@end
