6c2e11513d8b5ce387623eb08b0c53401df82351
Alex Jarvis <alex@panaxiom.co.uk>
2011-02-10 10:05:36 +0000

    Added the UserManager

---
+++ Meep/Classes/UserManager.h
@@ -0,0 +1,16 @@
+//
+//  UserManager.h
+//  Meep
+//
+//  Created by Alex Jarvis on 10/02/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UserManager : NSObject {
+
+}
+
+@end
---
+++ Meep/Classes/UserManager.m
@@ -0,0 +1,14 @@
+//
+//  UserManager.m
+//  Meep
+//
+//  Created by Alex Jarvis on 10/02/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserManager.h"
+
+
+@implementation UserManager
+
+@end
