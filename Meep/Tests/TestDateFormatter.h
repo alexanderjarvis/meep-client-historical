6c1d9cdded44f9aab6d5d9a5ae59b471da755bca
Alex Jarvis <alex@panaxiom.co.uk>
2011-03-29 15:16:04 +0000

    Added Tests directory

---
+++ Meep/Tests/TestDateFormatter.h
@@ -0,0 +1,17 @@
+//
+//  TestDateFormatter.h
+//  Meep
+//
+//  Created by Alex Jarvis on 29/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+
+@interface TestDateFormatter : SenTestCase {
+    
+}
+
+@end
---
+++ Meep/Tests/TestDateFormatter.m
@@ -0,0 +1,38 @@
+//
+//  TestDateFormatter.m
+//  Meep
+//
+//  Created by Alex Jarvis on 29/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestDateFormatter.h"
+
+#import "DateFormatter.h"
+
+@implementation TestDateFormatter
+
+- (void)testStringFromDateWithTimeZone {
+    [NSTimeZone setDefaultTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"BST"]];
+    [NSTimeZone resetSystemTimeZone];
+    NSDate *dateNow = [NSDate date];
+    
+    NSLog(@"date now: %@", dateNow);
+    NSString *dateNowString = [DateFormatter stringFromDate:dateNow];
+    NSLog(@"date now ISO string: %@", dateNowString);
+    
+    NSDate *dateFromString = [DateFormatter dateFromString:dateNowString];
+    NSLog(@"date now back from ISO string: %@", dateFromString);
+    
+    STAssertTrue([[dateNow description] isEqualToString:[dateFromString description]], @"Dates do not equal");
+}
+
+- (void)testDateFromStringWithTimeZone {
+    
+    NSDate *dateFromString = [DateFormatter dateFromString:@"2011-03-29T13:27:17+0100"];
+    NSString *expectedDateFromString = @"2011-03-29 12:27:17 +0000";
+    
+    STAssertEqualObjects([dateFromString description], expectedDateFromString, @"Dates do not equal");
+}
+
+@end
